b373663361c5a3de5822740273180bf6080636fe
komaldeep001 <81177755+komaldeep001@users.noreply.github.com>
2022-05-17 01:19:31 +0530

    Add files via upload

---
+++ Recursion/Lecture 1/Fraz_recursion_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    int partialAns = factorial(n - 1);
+    return n * partialAns;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 0)
+    {
+        cout << "Error";
+    }
+    else
+    {
+        cout << factorial(n);
+    }
+    return 0;
+}
