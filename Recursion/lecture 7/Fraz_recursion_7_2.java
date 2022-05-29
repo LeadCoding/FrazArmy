8be55149855da32a226bc491802b2f39da693314
Niraj Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2022-05-29 17:00:02 +0530

    Update Fraz_recursion_7_2.java
    Working java code for 2nd approach

--- Recursion/lecture 7/Fraz_recursion_7_2.java
+++ Recursion/lecture 7/Fraz_recursion_7_2.java
@@ -1,20 +1,28 @@
-ArrayList<ArrayList<Integer>> ans;
-ArrayList<Integer> subSet;
-
-void helper(ArrayList<Integer> v, int i, ArrayList<Integer> subSet, ArrayList<ArrayList<Integer>> ans) {
-        if (i == v.size()) {
-            ans.add(subSet);
+import java.util.ArrayList;
+public class Solution {
+       static ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+       static ArrayList<Integer> subSet=new ArrayList<Integer>();
+    
+    public static void helper(ArrayList<Integer> v, int i, ArrayList<Integer> subSet, ArrayList<ArrayList<Integer>> ans)
+    {
+        if (i == v.size())
+        {
+            ans.add(new ArrayList<Integer>(subSet));
             return;
         }
         // include the ith element
         subSet.add(v.get(i));
         helper(v, i + 1, subSet, ans);
         // not including the ith element
-        subSet.remove(i);
+        subSet.remove(subSet.size() - 1);
         helper(v, i + 1, subSet, ans);
 }
-
-ArrayList<ArrayList<Integer>> pwset(ArrayList<Integer> v) {
+    
+	public static ArrayList<ArrayList<Integer>> pwset(ArrayList<Integer> v) {
+		// WRITE YOUR CODE HERE
+     
         helper(v, 0, subSet, ans);
         return ans;
+	}
 }
+
