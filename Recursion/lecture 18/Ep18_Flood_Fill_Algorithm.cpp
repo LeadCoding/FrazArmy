2b7e8e095b5730114f872a51d309954a9e4f0a12
komaldeep001 <81177755+komaldeep001@users.noreply.github.com>
2022-06-23 22:48:27 +0530

    Update Ep18_Flood_Fill_Algorithm.cpp

--- Recursion/lecture 18/Ep18_Flood_Fill_Algorithm.cpp
+++ Recursion/lecture 18/Ep18_Flood_Fill_Algorithm.cpp
@@ -1,61 +1,21 @@
-// Approach 1 -
-void floodFillHelper(vector<vector<int>> &image, int i, int j, int oldColor, int newColor) {
-    if (i < 0 || i >= image.size() || j < 0 || j >= image[0].size()) {
-        return;
-    }
+#include<bits/stdc++.h>
+using namespace std;
 
-    if (image[i][j] != oldColor) {
-        return;
-    }
-
-    if (image[i][j] == newColor) {
-        return;
-    }
-
+void flood(int i, int j, vector<vector<int>> &image, int oldColor, int newColor, int n, int m) {
+    
+    if(i < 0 || j < 0 || i == n || j == m || image[i][j] != oldColor) return;
     image[i][j] = newColor;
-
-    floodFillHelper(image, i, j + 1, oldColor, newColor);
-    floodFillHelper(image, i, j - 1, oldColor, newColor);
-    floodFillHelper(image, i + 1, j, oldColor, newColor);
-    floodFillHelper(image, i - 1, j, oldColor, newColor);
+    flood(i + 1, j, image, oldColor, newColor, n, m);
+    flood(i - 1, j, image, oldColor, newColor, n, m);
+    flood(i, j + 1, image, oldColor, newColor, n, m);
+    flood(i, j - 1, image, oldColor, newColor, n, m);
 }
 
 vector<vector<int>> floodFill(vector<vector<int>> &image, int x, int y, int newColor) {
+    // Write your code here.
     int oldColor = image[x][y];
-    floodFillHelper(image, x, y, oldColor, newColor);
-    return image;
-}
-
-// Approach 2 -
-vector<vector<int>> floodFill(vector<vector<int>> &image, int x, int y, int newColor)
-{
-    int oldColor = image[x][y];
-    int m = image.size();
-    int n = image[0].size();
-    queue<pair<int, int>> q;
-
-    q.push({x, y});
-
-    while (!q.empty())
-    {
-        pair<int, int> currentPixel = q.front();
-        q.pop();
-
-        int i = currentPixel.first;
-        int j = currentPixel.second;
-
-        if (i >= 0 && i < m && j >= 0 && j < n)
-        {
-            if (image[i][j] == oldColor && image[i][j] != newColor) 
-            {
-                image[i][j] = newColor;
-                q.push({i, j + 1});
-                q.push({i, j - 1});
-                q.push({i + 1, j});
-                q.push({i - 1, j});
-            }
-        }
-    }
-
+    if(oldColor == newColor) return image;
+    int n = image.size(), m = image[0].size();
+    flood(x, y, image, oldColor, newColor, n, m);
     return image;
 }
