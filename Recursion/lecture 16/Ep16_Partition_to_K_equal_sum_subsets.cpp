7b790c4a4aad0e3daf7dbf5298ec95496e115653
komaldeep001 <81177755+komaldeep001@users.noreply.github.com>
2022-06-23 22:47:36 +0530

    Update Ep16_Partition_to_K_equal_sum_subsets.cpp

--- Recursion/lecture 16/Ep16_Partition_to_K_equal_sum_subsets.cpp
+++ Recursion/lecture 16/Ep16_Partition_to_K_equal_sum_subsets.cpp
@@ -1,87 +1,36 @@
-// Approach 1 --
-bool canPartitionKSubsetsHelper(vector<int> &nums, int K, vector<bool> &vis, int targetSubsetSum, int curSubsetSum, int checkIdx) 
-{
-	if (K == 0) 
-    {
-		return true;
-	}
+#include<bits/stdc++.h>
+using namespace std;
 
-	if (curSubsetSum == targetSubsetSum) 
-    {
-		return canPartitionKSubsetsHelper(nums, K - 1, vis, targetSubsetSum, 0, 0);
-	}
+bool help(int i, int bucketNum, int bucketSum, int reqSum, int K, vector<int> &nums, vector<int> &alreadyPicked) {
+    if(bucketNum == K + 1) return true;
+    if(bucketSum == reqSum) {
+        return help(0, bucketNum + 1, 0, reqSum, K, nums, alreadyPicked);
+    }
 
-	for (int i = checkIdx; i < nums.size(); i++) 
-    {
-		if (vis[i] == false && curSubsetSum + nums[i] <= targetSubsetSum) 
-        {
-			vis[i] = true;
-			if (canPartitionKSubsetsHelper(nums, K, vis, targetSubsetSum, curSubsetSum + nums[i], i + 1)) 
-            {
-				return true;
-			}
+    if(bucketSum > reqSum) return false;
+    if(i >= nums.size()) return false;
+    if(alreadyPicked[i] == 1) {
+        return help(i + 1, bucketNum, bucketSum, reqSum, K, nums, alreadyPicked);
+    }
+    else {
+        bucketSum += nums[i];
+        alreadyPicked[i] = 1;
+        bool op1 = help(i + 1, bucketNum, bucketSum, reqSum, K, nums, alreadyPicked);
 
-			vis[i] = false;
-		}
-	}
-	return false;
+        bucketSum -= nums[i];
+        alreadyPicked[i] = 0;
+        bool op2 = help(i + 1, bucketNum, bucketSum, reqSum, K, nums, alreadyPicked);
+
+        return op1 | op2;
+    }
 }
 
-bool canPartitionKSubsets(vector<int> &nums, int N, int K) 
-{
-	int sum = 0;
-	int maxNum = 0;
-	for (int i = 0; i < nums.size(); i++) 
-    {
-		sum += nums[i];
-		maxNum = max(maxNum, nums[i]);
-	}
-
-	if (sum % K != 0 || maxNum > sum / K) 
-    {
-		return false;
-	}
-
-    vector<bool> vis(nums.size(),false);
-	return canPartitionKSubsetsHelper(nums, K, vis, sum / K, 0, 0);
-}
-
-
-// Approach 2 --
-bool canPartitionKSubsets(vector<int>& nums, int n, int K)
-{
-    int dp[(1 << n) + 1];
-    memset(dp, -1, sizeof dp);
-
-    int tot = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        tot += nums[i];
-    }
-
-    if (tot % K != 0) {
-        return false;
-    }
-
-    int eachSum = tot / K;
-
-    dp[0] = 0;
-    for (int mask = 0; mask < (1 << n); mask++) {
-        if (dp[mask] == -1) {
-            continue;
-        }
-
-        for (int i = 0; i <= n - 1; i++) {
-            if ( !(mask & (1 << i)) &&  dp[mask] + nums[i] <= eachSum) {
-                dp[mask | (1 << i)] = dp[mask] + nums[i];
-                dp[mask | (1 << i)] %= eachSum;
-                if (dp[mask | (1 << i)] == 0 && (mask | (1 << i)) == ((1 << n) - 1)) {
-                    return true;
-                }
-            }
-
-        }
-
-    }
-
-    return false;
+bool canPartitionKSubsets(vector<int>& nums, int n, int K) {
+    // Write your code here.
+    vector<int> alreadyPicked(n, 0);
+    int sum = 0;
+    for(int i = 0; i < n; i++) sum += nums[i];
+    if(sum % K != 0) return false;
+    int reqSum = sum / K;
+    return help(0, 1, 0, reqSum, K, nums, alreadyPicked);
 }
